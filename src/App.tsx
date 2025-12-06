fd3af0e99318894a9ae9ff79a8741fdc9d7a51d5
marco-pontes <aurelio.pontes@gmail.com>
2025-12-06 19:04:48 -0300

    Adjusting genreral style and testing with several components to look like the nayax style guide. Added several components from shadcn and the datatable from syncfusion

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { type ReactNode } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button.tsx";
+import { Textarea } from "@/components/ui/textarea.tsx";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select.tsx";
+import SimpleSyncFusionTable from "@/components/SimpleSyncFusionTable.tsx";
+import { Checkbox } from "@/components/ui/checkbox.tsx";
+import { Label } from "./components/ui/label";
+import { AppForm } from "@/components/AppForm.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Navbar({ children }: { children: ReactNode }) {
+  return children;
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <header>
+        <h1>My Website Title</h1>
+        <Navbar>
+          <nav className="bg-[#262626]">
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+              AAAA
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </nav>
+        </Navbar>
+      </header>
+      {/*<aside>*/}
+      {/*  <SidebarProvider>*/}
+      {/*    <AppSidebar />*/}
+      {/*    <SidebarTrigger />*/}
+      {/*  </SidebarProvider>*/}
+      {/*</aside>*/}
+      <main>
+        <h4>Related Links</h4>
+        <section>
+          <Button>My custom button</Button>
+          <div className="flex flex-col gap-6">
+            <div className="flex items-center gap-3">
+              <Checkbox id="terms" />
+              <Label htmlFor="terms">Accept terms and conditions</Label>
+            </div>
+            <div className="flex items-start gap-3">
+              <Label htmlFor="terms-2">Accept terms and conditions</Label>
+              <Checkbox id="terms-2" defaultChecked />
+            </div>
+            <div className="flex items-start gap-3">
+              <Label htmlFor="toggle">Enable notifications</Label>
+              <Checkbox id="toggle" disabled />
+            </div>
+          </div>
+
+          <Textarea></Textarea>
+
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select a fruit" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Fruits</SelectLabel>
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <AppForm />
+          <SimpleSyncFusionTable />
+        </section>
+      </main>
+
+      <footer>
+        <figure>
+          <img src="logo.png" alt="Company Logo" />
+          <figcaption>Our Company Logo</figcaption>
+        </figure>
+        <p>&copy; 2025 Semantic Web Company. All rights reserved.</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SimpleSyncFusionTable.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Inject,
+  Page,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+import { orderDetails, type Order } from "./mockData";
+
+const SimpleSyncfusionTable: React.FC = () => {
+  const data: Order[] = orderDetails;
+
+  const pageSettings = { pageSizes: true, pageSize: 5 };
+  const filterSettings = { type: "Excel" as const }; // Define o tipo de filtro
+
+  return (
+    <div style={{ margin: "20px" }}>
+      <h2>Syncfusion DataGrid</h2>
+      <GridComponent
+        dataSource={data}
+        allowPaging={true}
+        pageSettings={pageSettings}
+        allowSorting={true}
+        allowFiltering={true}
+        filterSettings={filterSettings}
+        height={315}
+      >
+        <ColumnsDirective>
+          <ColumnDirective
+            field="OrderID"
+            headerText="ID do Pedido"
+            width="100"
+            textAlign={"Right"}
+          />
+          <ColumnDirective
+            field="CustomerName"
+            headerText="Nome do Cliente"
+            width="150"
+          />
+          <ColumnDirective
+            field="Freight"
+            headerText="Frete"
+            width="100"
+            format="C2"
+            textAlign={"Right"}
+          />
+          <ColumnDirective
+            field="ShipCountry"
+            headerText="País de Envio"
+            width="120"
+          />
+        </ColumnsDirective>
+        <Inject services={[Page, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default SimpleSyncfusionTable;
