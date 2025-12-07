db6a4d8f3a6da986aad29bcfdf1fcea135bb4e63
marco-pontes <aurelio.pontes@gmail.com>
2025-12-07 13:51:27 -0300

    Obtained and registered license for the syncfusion datatable component that will be used later.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { registerLicense } from "@syncfusion/ej2-base";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1JFaF1cXGFCf0xzWmFZfVhgdV9EZ1ZQRmY/P1ZhSXxWd0djX35XcXVRTmVeUkB9XEM=",
+);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
